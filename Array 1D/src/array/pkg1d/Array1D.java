ed52cfe9aa690d30148612276e51cf61f5ed5893
DimasFannyAzzuzi <dimasf.a46590@gmail.com>
2021-03-23 20:04:09 +0700

    Tugas week 3

---
+++ Array 1D/src/array/pkg1d/Array1D.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array.pkg1d;
+
+/**
+ *
+ * @author Riska
+ */
+public class Array1D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] teks = {"Dimas","Fany","Azzuzi"};
+        
+        for(int j=0; j<teks.length; j++) {
+            System.out.println(teks[j]);
+            System.out.println(" ");
+        }
+    }
+    
+}
