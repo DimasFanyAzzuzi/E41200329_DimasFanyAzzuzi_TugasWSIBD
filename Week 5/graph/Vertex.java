e86ff2d0c88960685af0e9dfc0e89bdafa2a4bb3
Dimas Fany Azzuzi <80267664+DimasFanyAzzuzi@users.noreply.github.com>
2021-04-25 06:30:10 +0700

    Add files via upload

---
+++ Week 5/graph/Vertex.java
@@ -0,0 +1,24 @@
+
+package graph;
+
+/**
+ *
+ * @author WinSeven
+ */
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex(char label) {
+        this.label = label; 
+        wasVisited = false;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
